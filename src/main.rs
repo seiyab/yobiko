b8cbb4a06b3aff0899589abefffba7960856b0d7
seiya <20365512+seiyab@users.noreply.github.com>
2026-07-09 22:16:00 +0000

    tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use ratatui::{DefaultTerminal, Frame};
+mod ui;
+
+use ratatui::DefaultTerminal;
 
 fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+    // color_eyre::install()?;
     ratatui::run(app)?;
     Ok(())
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+    ui::app::App::default().run(terminal)
 }
