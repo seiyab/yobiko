3d0d092d2b2f60a0abd671c073ac47d6ce5dc8ca
seiya <20365512+seiyab@users.noreply.github.com>
2026-07-10 14:32:25 +0000

    color-eyre

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod ui;
 use ratatui::DefaultTerminal;
 
 fn main() -> color_eyre::Result<()> {
-    // color_eyre::install()?;
+    color_eyre::install()?;
     ratatui::run(app)?;
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+fn app(terminal: &mut DefaultTerminal) -> color_eyre::Result<()> {
     ui::app::App::default().run(terminal)
 }
