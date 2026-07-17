73e06f1d28a84aba1ef46fb4900166477ee65985
seiya <20365512+seiyab@users.noreply.github.com>
2026-07-17 09:48:16 +0000

    test ecq

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { useApp, Box, Text, useInput } from "ink";
+import { useQuery } from "@tanstack/react-query";
+import { ecq } from "@seiyab/ecq";
+import { promises as promisesFs } from "node:fs";
+
+const fs = ecq.client(promisesFs);
 
 export function App() {
 	const { exit } = useApp();
@@ -8,9 +13,11 @@ export function App() {
 			exit();
 		}
 	});
+	const p = useQuery(fs.readFile("./package.json", "utf8"));
 	return (
 		<Box flexDirection="column" alignItems="stretch" width="100%" height="100%">
 			<Text>Hello</Text>
+			<Text>{p.isPending ? "pending" : p.isSuccess ? p.data : "error"}</Text>
 		</Box>
 	);
 }
